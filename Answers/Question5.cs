a619a84c23ccd7a85c474d1d580702f84f8f82d8
Sam Gao <gaoshan_274@hotmail.com>
2018-10-29 02:21:30 +0000

    pin array

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -7,22 +7,25 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         public static unsafe int Answer(int[] numOfShares, int totalValueOfShares)
         {
             if (totalValueOfShares < 0) return 0;
-            int nx, g, i, j, t, len = numOfShares.Length;
-            int* ans = stackalloc int[totalValueOfShares + 5];
-            ans[0] = 0;
-            for (i = 1; i <= totalValueOfShares; i++) ans[i] = 1 << 30;
-        
-            for (i = 0; i < len; i++)
+            fixed (int* shares = numOfShares)
             {
-                g = numOfShares[i];
-                for (j = g, t = 0; j <= totalValueOfShares; j++, t++)
+                int nx, g, i, j, t, len = numOfShares.Length;
+                int* ans = stackalloc int[totalValueOfShares + 5];
+                ans[0] = 0;
+                for (i = 1; i <= totalValueOfShares; i++) ans[i] = 1 << 30;
+            
+                for (i = 0; i < len; i++)
                 {
-                    nx = ans[t] + 1;
-                    if (nx < ans[j]) ans[j] = nx;
+                    g = shares[i];
+                    for (j = g, t = 0; j <= totalValueOfShares; j++, t++)
+                    {
+                        nx = ans[t] + 1;
+                        if (nx < ans[j]) ans[j] = nx;
+                    }
                 }
+                if (ans[totalValueOfShares] < 1 << 30) return ans[totalValueOfShares];
+                return 0;
             }
-            if (ans[totalValueOfShares] < 1 << 30) return ans[totalValueOfShares];
-            return 0;
         }
     }
 }
