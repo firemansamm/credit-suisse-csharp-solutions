3a6783373c3bca6c1a437032cc8ca7306ce7f093
Sam Gao <gaoshan_274@hotmail.com>
2018-10-29 00:47:23 +0000

    uhm

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -14,21 +14,22 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 n = _n;
             }
         }
-        unsafe class PQ
+        static unsafe class PQ
         {
-            readonly State* s;
-            public int edx = 1;
-            readonly int* ixs;
+            static State* s;
+            public static int edx;
+            static int* ixs;
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public PQ(State* _s, int* _ixs)
+            public static Init(State* _s, int* _ixs)
             {
                 s = _s;
                 ixs = _ixs;
+                edx = 1;
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            void r_up(int idx)
+            static void r_up(int idx)
             {
                 int p = idx / 2;
                 while (idx != 1)
@@ -43,7 +44,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            void r_down(int idx)
+            static void r_down(int idx)
             {
                 int nx = idx * 2;
                 while (nx < edx)
@@ -59,7 +60,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Push(int n, int p)
+            public static void Push(int n, int p)
             {
                 s[edx] = new State(p, n);
                 ixs[n] = edx;
@@ -68,7 +69,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Update(int n, int p)
+            public static void Update(int n, int p)
             {
                 int idx = ixs[n];
                 int cp = s[idx].p;
@@ -87,7 +88,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public int Pop(out int p)
+            public static int Pop(out int p)
             {
                 State ret = s[1];
                 s[1] = s[edx - 1];
@@ -99,7 +100,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public bool Contains(int n)
+            public static bool Contains(int n)
             {
                 return (ixs[n] != 0);
             }
@@ -110,7 +111,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             int sz = numOfServers + 10;
             State* s = stackalloc State[sz];
             int* ixs = stackalloc int[sz];
-            PQ pq = new PQ(s, ixs);
+            PQ.Init(s, ixs);
             bool* v = stackalloc bool[sz];
             int* sp = stackalloc int[sz];
             /* pre create state objects */
@@ -119,11 +120,11 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 sp[i] = int.MaxValue;
             }
             sp[0] = 0;
-            pq.Push(0, 0);
+            PQ.Push(0, 0);
             int cn, p;
-            while (pq.edx != 1)
+            while (PQ.edx != 1)
             {
-                cn = pq.Pop(out p);
+                cn = PQ.Pop(out p);
                 if (cn == targetServer) return p;
                 if (v[cn]) continue;
                 v[cn] = true;
@@ -133,8 +134,8 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                     int np = sp[cn] + connectionTimeMatrix[cn, i];
                     if (np < sp[i])
                     {
-                        if (pq.Contains(i)) pq.Update(i, np);
-                        else pq.Push(i, np);
+                        if (PQ.Contains(i)) PQ.Update(i, np);
+                        else PQ.Push(i, np);
                         sp[i] = np;
                     }
                 }
