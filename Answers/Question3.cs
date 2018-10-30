739fb73dd985477dd1ec1930d271c7f708ee3c9b
Sam Gao <gaoshan_274@hotmail.com>
2018-10-30 00:50:13 +0000

    eek

--- Answers/Question3.cs
+++ Answers/Question3.cs
@@ -6,16 +6,16 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question3
     {
-        static unsafe int dfs(int n, bool* v, List<int>[] ad, int* order, int* c, int* nc)
+        static unsafe int dfs(int n, bool* v, int* ad, int* order, int* c, int* nc, int tn)
         {
             if (v[n]) return 0;
             v[n] = true;
             /* 0: color this */
             /* 1: don't color this */
-            int ret = 0, flag = 1, o;
-            List<int> cx = ad[n];
-            for (int i = 0; i < cx.Count; i++)
+            int ret = 0, flag = 1, o, adj = ad[n];
+            for (int i = 1; i <= tn; i++)
             {
+                if ((adj & (1 << i)) == 0) continue;
                 o = cx[i];
                 if (v[o])
                 {
@@ -23,7 +23,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                     continue;
                 }
                 order[o] = order[n] + 1;
-                ret += dfs(o, v, ad, order, c, nc);
+                ret += dfs(o, v, ad, order, c, nc, tn);
                 c[n] += nc[o];
                 if (c[o] > nc[o]) nc[n] += c[o];
                 else nc[n] += nc[o];
@@ -36,28 +36,22 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         {
             /* build adjlist */
             int vv = numOfNodes + 5;
-            List<int>[] ad = new List<int>[vv];
             bool* v = stackalloc bool[vv];
-            int* order = stackalloc int[vv], c = stackalloc int[vv], nc = stackalloc int[vv];
+            int* order = stackalloc int[vv], c = stackalloc int[vv], nc = stackalloc int[vv], ad = stackalloc int[vv];
             int el = edgeLists.Length;
-            /* tune the adjlist? maybe list access is slow */
             /* for n <= 32, use a bitmask */
-            for (int i = 0; i <= numOfNodes; i++)
-            {
-                ad[i] = new List<int>();
-            }
             for (int i = 0; i < el; i++)
             {
                 Edge e = edgeLists[i];
                 int x = e.EdgeA, y = e.EdgeB;
-                ad[x].Add(y);
-                ad[y].Add(x);
+                ad[x] |= 1<<y;
+                ad[y] |= 1<<x;
             }
             int ans = 0, f, g;
             for (int i = 1; i <= numOfNodes; i++)
             {
                 if (v[i]) continue;
-                f = dfs(i, v, ad, order, c, nc);
+                f = dfs(i, v, ad, order, c, nc, numOfNodes);
                 g = c[i];
                 if (nc[i] > g) g = nc[i];
                 g = g + g - f;
