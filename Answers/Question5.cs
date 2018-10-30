ee7cd21cd997750a14e3a2d325b374b56909e8b6
Sam Gao <gaoshan_274@hotmail.com>
2018-10-30 14:26:44 +0000

    apparently this is faster??

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -4,39 +4,25 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question5
     {
-         /* how much faster is recursion */
-            public static unsafe int rec(int* coins, int idx, int val, int[] memo, int rs)
-            {
-                if (val < 0) return 1 << 30;
-                if (val == 0) return 0;
-                int ix = (idx * rs) + val;
-                if (memo[ix] != 0) return memo[ix];
-                int cc = coins[idx], ct = val / cc;
-                if (val % cc == 0) return ct;
-                if (idx == 0) return 1 << 30;
-                int ans = 1<<30, nv = coins[idx-1], rem;
-                if (nv == 0) return 1 << 30;
-                for (int i = ct; i >= 0; i--)
-                {
-                    rem = val - (i * cc);
-                    if (i + ((rem + nv - 1) / nv) >= ans) return memo[ix] = ans;
-                    ans = Math.Min(ans, i + rec(coins, idx - 1, rem, memo, rs));
-                }
-                return memo[ix] = ans;
-            }
+        public static unsafe int Answer2(int[] numOfShares, int totalValueOfShares)
+        {
+            if (totalValueOfShares < 0) return 0;
+            int nx, g, i, j, t, len = numOfShares.Length;
+            int[] ans = new int[totalValueOfShares + 5];
+            ans[0] = 0;
+            for (i = 1; i <= totalValueOfShares; i++) ans[i] = 1 << 30;
 
-            public static unsafe int Answer(int[] numOfShares, int totalValueOfShares)
+            for (i = 0; i < len; i++)
             {
-                if (totalValueOfShares < 0) return 0;
-                Array.Sort(numOfShares);
-                int len = numOfShares.Length;
-                int[] mem = new int[len * (totalValueOfShares + 1)];
-                fixed (int* shares = numOfShares)
+                g = numOfShares[i];
+                for (j = g, t = 0; j <= totalValueOfShares; j++, t++)
                 {
-                    int ans = rec(shares, len - 1, totalValueOfShares, mem, totalValueOfShares);
-                    if (ans < 1 << 30) return ans;
-                    else return 0;
+                    nx = ans[t] + 1;
+                    if (nx < ans[j]) ans[j] = nx;
                 }
             }
+            if (ans[totalValueOfShares] < 1 << 30) return ans[totalValueOfShares];
+            return 0;
+        }
     }
 }
