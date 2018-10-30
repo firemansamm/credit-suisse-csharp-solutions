2292ffb101467043d4412cca6a5a47eb2ca17164
Sam Gao <gaoshan_274@hotmail.com>
2018-10-30 00:47:33 +0000

    small

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -8,18 +8,21 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 
         public static unsafe int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
-            int ans = 1 << 30, sm = 0, sz = 0, l1 = machineToBeFixed.GetLength(0), l2 = machineToBeFixed.GetLength(1), lx = 0, vf = numOfConsecutiveMachines - 1, r = 0;
+            int ans = 1 << 30, sm = 0, sz = 0, l1 = machineToBeFixed.GetLength(0), l2 = machineToBeFixed.GetLength(1), 
+                lx = 0, vf = numOfConsecutiveMachines - 1, r = 0, brk = l2 - numOfConsecutiveMachines;
             int* vs = stackalloc int[l2];
             for (int i = 0; i < l1; i++)
             {
                 for (int j = 0; j < l2; j++)
                 {
                     /* manually inline it since the IL compiler wont */
+                    /* only do the conversion if you can find k in a row */
                     r = 0;
                     fixed (char* g = machineToBeFixed[i, j])
                     {
                         char* t = g;
                         if (*t == 'X') {
+                            if (j >= brk) break;
                             sz = 0;
                             sm = 0;
                             lx = j + 1;
