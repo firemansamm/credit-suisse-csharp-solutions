8f3ec64766e8d275bd26d6b55ffb75208a3e729f
Sam Gao <gaoshan_274@hotmail.com>
2018-10-31 01:26:48 +0000

    weird thing

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -7,15 +7,14 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         public static unsafe int Answer(int[] numOfShares, int totalValueOfShares)
         {
             if (totalValueOfShares < 0) return 0;
-            int nx, g, i, j, t, len = numOfShares.Length;
+            int nx, i, j, t, len = numOfShares.Length;
             int[] ans = new int[totalValueOfShares + 1];
             ans[0] = 0;
             for (i = 1; i <= totalValueOfShares; i++) ans[i] = 1 << 30;
 
             for (i = 0; i < len; i++)
             {
-                g = numOfShares[i];
-                for (j = g, t = 0; j <= totalValueOfShares; j++, t++)
+                for (j = numOfShares[i], t = 0; j <= totalValueOfShares; j++, t++)
                 {
                     nx = ans[t] + 1;
                     if (nx < ans[j]) ans[j] = nx;
